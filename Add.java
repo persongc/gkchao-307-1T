1e2fb6f69c42beb315538820b5129f7dc742c5d2
persongc <gavin.chao123@gmail.com>
2018-10-05 14:37:28 -0700

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,19 @@
+package term1;
+
+public class Add {
+	
+	public static int sum(int one, int two)
+	{
+		return (one+two);
+	}
+	
+	public static void main(String[] args) {
+		
+		int one = 1;
+		int two = 2;
+		
+		System.out.print("1+2 = ");
+		System.out.println(sum(one,two));
+	}
+
+}
